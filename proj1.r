5f9193719edcdf844f622fa9e54b9a9733ba4e70
yhd <yuehuaidong0210@gmail.com>
2025-09-22 13:30:35 +0100

    add split assignments

--- proj1.r
+++ proj1.r
@@ -1,7 +1,7 @@
 # Member1：Chunxi Su（UNN：s2814164）；Member2：Huaidong Yue（UNN：s2815318）；Yifei Peng：王五（UNN：s2792136）
 # Contribution Instruction：
 
-# Project Instruction：
+## Project Instruction：Member A (33%): Text Preparation and Preprocessing Module；Member B (34%): Word Frequency Statistics and Sequence Matrix Construction Module；Member C (33%): Core function development and sentence generation module
 
 #setwd("/Users/clggmac/Rprogramming/Shakespeare-project/shakespeare-text-project18")  ## The setwd line should be commented out of the code you finally submit for marking！！！
 # reading text：skip first 83 rows（invalid instruction）,reading 196043-83 row（valid text）,coding with UTF-8
